b91c65e542ae3814668c935ad34333fd211e0c57
Elvira Joubert <reagent01@gmail.com>
2019-03-28 20:07:14 -0500

    commit init

---
+++ testConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey there World!");
+            Thread.Sleep(3000);
+            Console.ReadLine();
+        }
+    }
+}
